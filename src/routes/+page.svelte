845672d79386cf22ccc74ae55a649f8c8a276ee7
Michel <mfagundesleal@outlook.com>
2022-10-05 16:39:26 -0300

    Assets locais ao invés do hygraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 </svelte:head>
 
 <main class="flex flex-col gap-20">
-    <SectionBgImg bgImage="https://media.graphassets.com/Bw2aguyyQlqjFezaPiAD" classes="bg-cover bg-left xl:bg-right h-[28.125rem] xl:h-600">
+    <SectionBgImg bgImage="/img/hero.webp" classes="bg-cover bg-left xl:bg-right h-[28.125rem] xl:h-600">
         <WrapContent classes="gap-1">
             <h2 class="font-panton font-light">Aprimore sua vida FInanceira</h2>
             <h1 class="font-panton font-bold text-3xl xl:text-5xl xl:w-2/3">Profissionalize-se, conheça nossos cursos para o mercado de trabalho</h1>
@@ -35,16 +35,16 @@
     
     <section>
         <WrapGd classes="min-w-272 md:grid-cols-2 xl:grid-cols-3">
-            <Card srcBgCard="https://media.graphassets.com/nM6lJbAlRhGfYRW4zq2H" titleCard="Profissões de supermercado"/>
-            <Card srcBgCard="https://media.graphassets.com/o3kDTJKLTjOlTC8N6QuF" titleCard="Montagem e manutenção de computadores e redes"/>
-            <Card srcBgCard="https://media.graphassets.com/ndn759r3T1W0GcXjflZO" titleCard="Consultor comercial"/>
-            <Card srcBgCard="https://media.graphassets.com/ZTiy5KP8R6a6QZSID16y" titleCard="Auxiliar de farmácia"/>
-            <Card srcBgCard="https://media.graphassets.com/5GlvrjX1Qc63KFoe7cm4" titleCard="Recepção hoteleira"/>
-            <Card srcBgCard="https://media.graphassets.com/Hk0beJXORYGghVbUmPTy" titleCard="Gestão empresarial"/>
+            <Card srcBgCard="/img/profissoes_supermercado.webp" titleCard="Profissões de supermercado"/>
+            <Card srcBgCard="/img/montmnt_comp.webp" titleCard="Montagem e manutenção de computadores e redes"/>
+            <Card srcBgCard="/img/consultor_comercial.webp" titleCard="Consultor comercial"/>
+            <Card srcBgCard="/img/auxiliar_farmacia.webp" titleCard="Auxiliar de farmácia"/>
+            <Card srcBgCard="/img/recep_hotel.webp" titleCard="Recepção hoteleira"/>
+            <Card srcBgCard="/img/gestao_empresarial.webp" titleCard="Gestão empresarial"/>
         </WrapGd>
     </section>
     
-    <SectionBgImg bgImage="https://media.graphassets.com/p5zs4XB2SVuW1dylPkYV" classes="bg-cover h-[28.125rem] xl:h-600">
+    <SectionBgImg bgImage="/img/bg-sobre.webp" classes="bg-cover h-[28.125rem] xl:h-600">
         <WrapContent>
             <h1 class="font-panton font-bold text-2xl xl:text-5xl w-11/12">Somos uma empresa profissional com mais de 20 anos de experiência no mercado</h1>
             <Button
@@ -65,17 +65,17 @@
                 <Accordion tituloAccordion="Premiações">
                     <WrapGrid slot="container" classes="lg:grid-cols-2 xl:grid-cols-3">
                         <Card 
-                            srcImageCard="https://media.graphassets.com/S4TSZ00dQHOOC7Mmdulg"
+                            srcImageCard="/img/premio-master-2008.webp"
                             titleCard="2008 - Prêmio Nacional"
                             titleCardClasses="font-panton font-light"
                             descCard="Master Pesquisas"
                             descCardClasses="font-muli"/>
                         <Card 
-                            srcBgCard="https://media.graphassets.com/iT9bRaJ2TrmRaHvkvyK5"
+                            srcBgCard="/img/premio-isq_bid-2019.webp"
                             titleCard="2019 - International Star for Quality (ISQR)" titleCardClasses="font-panton font-light"
                             descCard="Business Initiative Directions (BID).<br>Genebra, Suíça" descCardClasses="font-muli"/>
                         <Card 
-                            srcBgCard="https://media.graphassets.com/2JOoaX6TrWQc7TiEq1gu"
+                            srcBgCard="/img/premio-qa_esqr-2021.webp"
                             titleCard="2021 - Quality Achievements Awards"
                             titleCardClasses="font-panton font-light"
                             descCard="European Society for Quality Research (ESQR).<br>Dubai, Emirados Árabes Unidos"
@@ -86,19 +86,19 @@
                     <WrapContent slot="container" classes="gap-10 items-center">
                         <CardDestaque
                             classes="border-azul4 shadow-azul4 shadow-card"
-                            src="https://media.graphassets.com/oyIb1N3uQE2wXgdBeWwD"
+                            src="/img/icon-missao.svg"
                             titleCard="Missão"
                             descCard="Formar pessoas de todas as idades para atuarem nas mais diversas áreas. Estimular o desenvolvimento do caráter e da personalidade do aluno assim como atributos sociais e de convivência."
                         />
                         <CardDestaque
                             classes="border-laranja shadow-laranja shadow-card--destaque"
-                            src="https://media.graphassets.com/RYeQkNgRYm7nDwqpsHvI"
+                            src="/img/icon-valores.svg"
                             titleCard="Valores"
                             descCard="Acreditamos no potencial de nossa equipe. Investimos no bem-estar de nossos colaborares e os incentivamos a crescer dia após dia. Somos referência na área de educação e premiados internacionalmente! Ética, profissionalismo e foco no crescimento são nossos valores primordiais."
                         />
                         <CardDestaque
                             classes="border-azul4 shadow-azul4 shadow-card"
-                            src="https://media.graphassets.com/k0XHk1qRUWclcxKYr85z"
+                            src="/img/icon-objetivos.svg"
                             titleCard="Objetivos"
                             descCard="Prestar um serviço de qualidade, inspirando e moldando pessoas para atingirem seus objetivos da melhor forma possível. Prezar pelo capricho e motivar as pessoas a fazerem o melhor possível, com foco na eficiência. Formar jovens e adultos aptos a distribuir e compartilhar conhecimento visando o bem em comum!"
                         />
@@ -108,7 +108,7 @@
         </WrapContent>
     </section>
     
-    <SectionBgImg bgImage="https://media.graphassets.com/JW5W1W4SsKnMfqoBK1vw" classes="bg-cover bg-center lg:bg-top lg:bg-contain">
+    <SectionBgImg bgImage="/img/bg-equipe.webp" classes="bg-cover bg-center lg:bg-top lg:bg-contain">
         <WrapContent classes="gap-10 items-center">
             <div>
                 <h2 class="font-panton font-light">Conheça nossa equipe</h2>
