c9cf135411294ca78d52ab4ec6591a4c4eb66083
Michel <mfagundesleal@outlook.com>
2022-10-07 11:54:27 -0300

    Correção atributos form

--- src/lib/components/form.svelte
+++ src/lib/components/form.svelte
@@ -5,6 +5,7 @@
     export let name="";
 </script>
 
-<form {name} method="POST" class="flex flex-col items-center justify-center min-w-[18.75rem] bg-cover bg-no-repeat rounded-xl border-t-laranja py-10 px-5 {classes}" data-netlify="true">
+<form {name} method="POST" class="flex flex-col items-center justify-center min-w-[18.75rem] bg-cover bg-no-repeat rounded-xl border-t-laranja py-10 px-5 {classes}" netlify>
+    <input type="hidden" name="form-name" value="{name}" />
     <slot></slot>
 </form>
